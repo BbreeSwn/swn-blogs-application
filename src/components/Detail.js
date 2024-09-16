94f9a7a12b0a84031123a0de94a52e9ba20b784e
BbreeSwn <suwanan.cs@hotmail.com>
2024-09-16 11:35:10 +0700

    feat : get blog data result by useState

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,20 +1,28 @@
 // ไฟล์ detail.js รับหน้าที่ในการแสดงรายละเอียดบทความที่สนใจ
 
-import { useEffect } from "react"
-import { useParams } from "react-router-dom"
-import blogs from "../data/blogs"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import blogs from "../data/blogs";
 
 export default function Detail() {
-  const {id} = useParams()
+  const { id } = useParams();
+  const [title, setTitle] = useState("");
+  const [image , setImage] = useState("")
+  const [content , setContent] = useState("")
+  const [author , setAuthor] = useState("")
   useEffect(() => {
     //ดึงข้อมูลบทความ
-   const result = blogs.find((item)=>item.id === parseInt(id))
-   console.log(result);
-
-  },[])
+    const result = blogs.find((item) => item.id === parseInt(id));
+    // ดึง ค่า state มาจาก result
+    setTitle(result.title)
+    setImage(result.image_url)
+    setContent(result.content)
+    setAuthor(result.author)
+    console.log(result);
+  }, []);
   return (
     <div>
-        <h2>รายละเอียดบทความ : {id}</h2>
+      <h2>รายละเอียดบทความ : {id}</h2>
     </div>
-  )
+  );
 }
