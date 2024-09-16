320d1f9c8c58a548557bc9ca83741423d7e2defb
BbreeSwn <suwanan.cs@hotmail.com>
2024-09-16 10:58:54 +0700

    feat : get blog data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-import About from "./components/About";
-import Blogs from "./components/Blogs";
-import Home from "./components/Home";
 import Routes from "./routes/Routes";
 
 function App() {
--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,8 +1,19 @@
+import blogs from "../data/blogs"
+import "./Blogs.css"
 
 export default function Blogs() {
   return (
-    <div>
-        <h1>This is all Blogs</h1>
+    <div className="blogs-container">
+        <article>
+          {blogs.map((item)=>(
+            <div className="card" key={item.id}>
+              <h2>{item.title}</h2>
+              <p>{item.content.substring(0,300)}</p>
+              <hr/>
+            </div>
+          ))}
+        </article>
+
     </div>
   )
 }
