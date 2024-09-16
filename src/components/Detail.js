916e558ca2e7d8e0f8a087fac3f63a31d5e37b75
BbreeSwn <suwanan.cs@hotmail.com>
2024-09-16 11:59:15 +0700

    feat : show blog content

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -3,26 +3,34 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import blogs from "../data/blogs";
+import "./Detail.css";
 
 export default function Detail() {
   const { id } = useParams();
   const [title, setTitle] = useState("");
-  const [image , setImage] = useState("")
-  const [content , setContent] = useState("")
-  const [author , setAuthor] = useState("")
+  const [image, setImage] = useState("");
+  const [content, setContent] = useState("");
+  const [author, setAuthor] = useState("");
   useEffect(() => {
     //ดึงข้อมูลบทความ
     const result = blogs.find((item) => item.id === parseInt(id));
     // ดึง ค่า state มาจาก result
-    setTitle(result.title)
-    setImage(result.image_url)
-    setContent(result.content)
-    setAuthor(result.author)
+    setTitle(result.title);
+    setImage(result.image_url);
+    setContent(result.content);
+    setAuthor(result.author);
     console.log(result);
   }, []);
   return (
-    <div>
-      <h2>รายละเอียดบทความ : {id}</h2>
+    <div className="container">
+      <div className="detail-container">
+        <h2 className="title">Title : {title} </h2>
+        <img src={image} alt={title} className="blog-image" />
+        <div className="blog-detail">
+          <strong>Author : {author}</strong>
+          <p>{content}</p>
+        </div>
+      </div>
     </div>
   );
 }
