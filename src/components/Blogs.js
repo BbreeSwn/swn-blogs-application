ed3e19315d9f1cb1301c44602728c8dfc4f8c3c1
BbreeSwn <suwanan.cs@hotmail.com>
2024-09-16 12:20:14 +0700

    feat : search content

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,12 +1,29 @@
 import { Link } from "react-router-dom";
 import blogs from "../data/blogs";
 import "./Blogs.css";
+import { useEffect, useState } from "react";
 
 export default function Blogs() {
+  const [search, setSearch] = useState("");
+  const [filterBlog , setFilterBlog] = useState([])
+  useEffect(() => {
+    //filter title : Eng blogs.filter((item)=>item.title.toLocaleLowerCase().includes(search))
+    const result = blogs.filter((item) => item.title.includes(search));
+    setFilterBlog(result);
+  }, [search]);
   return (
     <div className="blogs-container">
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="search content"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
       <article>
-        {blogs.map((item) => (
+        {filterBlog.map((item) => (
           <Link to={`/blog/${item.id}`} key={item.id}>
             <div className="card">
               <h2>{item.title}</h2>
